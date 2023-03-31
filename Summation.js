589f50f49ac63fd99a4bd14f033362e094807c9c
shirudechirayu <63559685+shirudechirayu@users.noreply.github.com>
2023-04-01 00:59:34 +0530

    Delete Summation.js

--- Summation.js
+++
@@ -1,8 +0,0 @@
-function Summation(Arr) {
-    var Sum = 0;
-    for (var i = 0; i < Arr.length; i++) {
-        Sum += Arr[i];
-    }
-    return Sum;
-}
-console.log("Addition is: " + Summation([23, 6, 7, 4, 5, 7]));
